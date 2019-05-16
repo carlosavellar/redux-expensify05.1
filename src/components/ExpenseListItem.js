dc5df7b84d71805508f877f830902803ed60051a
José Carlos Santos <carlos.avellar@gmail.com>
2019-05-16 16:49:59 -0300

    Link Edit

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,11 @@
 import { connect } from "react-redux";
 import { removeExpense } from "../actions/expense";
+import { Link } from "react-router-dom";
 import React from "react";
 
 const ExpenseListItem = ({ dispatch, id, description, note, createdAt }) => (
     <div>
-        <h2>{description}</h2>
+        <Link to={`/edit/${id}`}><h2>{description}</h2></Link>
         <div>{note}</div>
         <div>{createdAt}</div>
         <button
