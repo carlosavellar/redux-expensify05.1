d5365fb31e83d2dc1373dad0d560b0d9a001fe96
José Carlos Santos <carlos.avellar@gmail.com>
2019-05-30 15:26:40 -0300

    Filter by text ok

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
+import selectedExpenses from "../selectors/expense";
 const ExpenseList = (props) => (
 
     <div>
@@ -16,7 +17,7 @@ const ExpenseList = (props) => (
 );
 
 const mapStateToProps = (state) => ({
-    expenses: state.expenses
+    expenses: selectedExpenses(state.expenses, state.filters)
 });
 
 export default connect(mapStateToProps)(ExpenseList);
