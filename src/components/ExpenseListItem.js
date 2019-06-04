fcb914a8419a6e0ea10d60c5a32f7fac40f26a38
José Carlos Santos <carlos.avellar@gmail.com>
2019-06-04 12:26:18 -0300

    Formating Date and Number

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { removeExpense } from "../actions/expense";
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import numeral from 'numeral';
+import moment from "moment";
 const ExpenseListItem = ({ description, amount, note, id, createdAt }) => (
     <li>
         <Link to={`/edit/${id}`}><span><strong>{description}: </strong></span></Link>
-        <span>R$ {amount}</span><span>  |  </span>
-        <span>{createdAt}</span><span>  |  </span>
+        <span> R{numeral(amount).format("$0,0.00")}</span><span>  |  </span>
+        <span>{moment(createdAt).format("MMMM Do, YYYY")}</span><span>  |  </span>
         <span>{note}</span><span>  |  </span>
         <span>{id}</span><span>  |  </span>
 
