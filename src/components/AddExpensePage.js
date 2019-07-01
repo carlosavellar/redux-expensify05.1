e89afff9504e83c9901a6248f3e76a948dd5fb24
José Carlos Santos <carlos.avellar@gmail.com>
2019-07-01 14:39:56 -0300

    Removing Expenses

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,19 +1,11 @@
 // Teste Undo COmmit
 import React from 'react';
-import ExpenseForm from "./ExpenseForm";
-import { addExpense } from "../actions/expense";
-import { connect } from 'react-redux'
+
 
 const AddExpensePage = (props) => (
   <div>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push("/");
-      }
-      }
-    />
+
   </div>
 );
 
-export default connect()(AddExpensePage);
+export default AddExpensePage;
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,14 +1,10 @@
 // Teste Undo COmmit
 import React from 'react';
 import ExpenseList from "./ExpenseList";
-import ExpenseListFilter from "./ExpenseListFilter";
-import ExpensesSumary from "./ExpensesSymary";
 
 const ExpenseDashboardPage = () => (
   <div>
     <ExpenseList />
-    <ExpenseListFilter />
-    <ExpensesSumary />
   </div>
 );
 
